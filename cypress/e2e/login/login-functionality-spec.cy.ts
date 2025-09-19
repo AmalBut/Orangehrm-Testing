14509172e7179c3c993514c9e9d6d0a6bc4ef71b
Amal Butmah <amalbutmah376@gmail.com>
2025-09-19 21:34:08 +0300

    Modify login to POM

--- cypress/e2e/login/login-functionality-spec.cy.ts
+++ cypress/e2e/login/login-functionality-spec.cy.ts
@@ -1,68 +1,108 @@
+import { dashboardPage } from "../../support/pages/dashboard-page";
+import { loginPage } from "../../support/pages/login-page";
 
 describe('Login Cases', () => {
+
     beforeEach(()=>{
-        cy.visit("https://opensource-demo.orangehrmlive.com/");
+        cy.fixture('users').as('users');
+        cy.fixture('messages').as('messages');
+        loginPage.visit();
+        loginPage.isLoaded();
     })
 
     it('TC01: Should login successfuly with valid username and password', () => {
-        cy.get('input[name=username]').type("Admin");
-        cy.get('input[name=password]').type("admin123");
-        cy.get("button:submit").click();
-        cy.url().should("include","/dashboard");
+       
+        cy.get('@users').then((users:any)=>{
+            cy.login(users.valid.username, users.valid.password);
+        });
+
+        dashboardPage.isLoaded();
     });
 
     it('TC02: Should show an error for valid username and invalid password', () => {
-        cy.get('input[name=username]').type("Admin");
-        cy.get('input[name=password]').type("admin12345");
-        cy.get("button:submit").click();
-        cy.contains("Invalid credentials").should("be.visible");
+         cy.get('@users').then((users:any)=>{
+            cy.login(users.valid.username, users.invalid.password);
+        });
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertInvalidCredentials(messages.invalidCred);
+        });
     });
 
     it('TC03: Should show an error for invalid username and valid password', () => {
-        cy.get('input[name=username]').type("Admin12");
-        cy.get('input[name=password]').type("admin123");
-        cy.get("button:submit").click();
-        cy.contains("Invalid credentials").should("be.visible");
+        
+        cy.get('@users').then((users:any)=>{
+            cy.login(users.invalid.username, users.valid.password);
+        });
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertInvalidCredentials(messages.invalidCred);
+        });
     });
 
     it('TC04: Should show an error for invalid username and invalid password', () => {
-        cy.get('input[name=username]').type("Admin@12");
-        cy.get('input[name=password]').type("admin123@");
-        cy.get("button:submit").click();
-        cy.contains("Invalid credentials").should("be.visible");
+
+        cy.get('@users').then((users:any)=>{
+            cy.login(users.invalid.username, users.invalid.password);
+        });
+
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertInvalidCredentials(messages.invalidCred);
+        });
     });  
     
     it('TC05: Should show required message when username is empty and password is valid', () => {
-        cy.get('input[name=password]').type("admin123");
-        cy.get("button:submit").click();
-        cy.contains(".oxd-form-row","Username").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
+        cy.get('@users').then((users:any)=>{
+            loginPage.typePassword(users.valid.password);
+        });
+        loginPage.submit();
+        
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertRequiredAt(0,messages.requiresMsg);
+        });
     });
 
     it('TC06: Should show required message when username is empty and password is invalid', () => {
-        cy.get('input[name=password]').type("admin123@#");
-        cy.get("button:submit").click();
-        cy.contains(".oxd-form-row","Username").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
+
+        cy.get('@users').then((users:any)=>{
+            loginPage.typePassword(users.invalid.password);
+        });
+        loginPage.submit();
+
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertRequiredAt(0,messages.requiresMsg);
+        });
     }); 
     
     it('TC07: Should show required message when password is empty and username is valid', () => {
-        cy.get('input[name=username]').type("Admin");
-        cy.get("button:submit").click();
-        cy.contains(".oxd-form-row","Password").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
+        cy.get('@users').then((users:any)=>{
+            loginPage.typeUsername(users.valid.username);
+        });
+        loginPage.submit();
+        
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertRequiredAt(0,messages.requiresMsg);
+        });
     });
     
     it('TC08: Should show required message when password is empty and username is invalid', () => {
-        cy.get('input[name=username]').type("Admin@12");
-        cy.get("button:submit").click();
-        cy.contains(".oxd-form-row","Password").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
+        cy.get('@users').then((users:any)=>{
+            loginPage.typeUsername(users.invalid.username);
+        });
+        loginPage.submit();
+
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertRequiredAt(0,messages.requiresMsg);
+        });
     });
     
     it('TC09: Should show required messages when both username and password are empty', () => {
-        cy.get("button:submit").click();
-        cy.contains(".oxd-form-row","Username").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
-        cy.contains(".oxd-form-row","Password").find(".oxd-input-field-error-message").should("contain","Required").and("be.visible");
+        loginPage.submit();
+        cy.get('@messages').then((messages:any)=>{
+            loginPage.assertRequiredAt(0,messages.requiresMsg);
+            loginPage.assertRequiredAt(1,messages.requiresMsg);
+        });
     });
     
     it('TC10: Should mask password input by default', () => {
-       cy.get('input[name=password]').should("have.attr","type","password");
+        loginPage.passwordShouldBeMasked();
     });
 });
